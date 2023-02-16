426c487b69672b45d48034f3b79156d9096407d1
hienq-le <124135405+hienq-le@users.noreply.github.com>
2023-02-16 08:58:03 +0700

    Add files via upload

--- install_microsoft365_x86.py
+++ install_microsoft365_x86.py
@@ -4,15 +4,18 @@ The setup.exe is updated every 1 day after the 2nd Tuesday of a month, UTC+7
 """
 import os
 config_file_name = "configuration_microsoft365_x86.xml"
+main_file_name = "setup.exe"
 try:
 	config_file = open(config_file_name, "r")
 	config_file.close()
+	main_file = open(main_file_name, "r")
+	main_file.close()
 except FileNotFoundError:
 	print("Couldn\'t download or install Microsoft 365 because the configuration file is missing!")
 	exit()
 print("Downloading Microsoft 365")
-os.system(".\\setup.exe /download "+config_file_name)
+os.system(".\\"+main_file_name+" /download "+config_file_name)
 print("Downloaded Microsoft 365 successfully")
 print("Installing Microsoft 365")
-os.system(".\\setup.exe /configure "+config_file_name)
+os.system(".\\"+main_file_name+" /configure "+config_file_name)
 print("Installed Microsoft 365 successfully")

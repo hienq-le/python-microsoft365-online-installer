a1ace790302ac5d5f00b48a9060a4860a7f772b6
hienq-le <124135405+hienq-le@users.noreply.github.com>
2023-02-16 08:36:33 +0700

    Add files via upload

---
+++ install_microsoft365_x86.py
@@ -0,0 +1,18 @@
+"""
+Microsoft 365 Apps 32-bit Online Installer Written in Python and XML
+The setup.exe is updated every 1 day after the 2nd Tuesday of a month, UTC+7
+"""
+import os
+config_file_name = "configuration_microsoft365_x86.xml"
+try:
+	config_file = open(config_file_name, "r")
+	config_file.close()
+except FileNotFoundError:
+	print("Couldn\'t download or install Microsoft 365 because the configuration file is missing!")
+	exit()
+print("Downloading Microsoft 365")
+os.system(".\\setup.exe /download "+config_file_name)
+print("Downloaded Microsoft 365 successfully")
+print("Installing Microsoft 365")
+os.system(".\\setup.exe /configure "+config_file_name)
+print("Installed Microsoft 365 successfully")
